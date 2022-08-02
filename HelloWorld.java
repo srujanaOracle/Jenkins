3f3c602addfe1dc44f82403d7d6762de596fda42
srujanaOracle <110465312+srujanaOracle@users.noreply.github.com>
2022-08-02 16:36:06 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String [] s) {
+        System.out.println("Hello All");
+        for(int i = 0; i < 20; i++) {
+            System.out.println("Value of i :" + i);
+        }
+        System.out.println("Finished");
+    }
+}
